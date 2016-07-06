3de3a30d9daa3414ca1454fd9f47897d0506db5a
we123326841 <wanghao@wanghaodeMacBook-Pro-3.local>
2016-07-06 18:11:28 +0800

    带我去大青蛙
    达到完全

--- xixi/count.js
+++ xixi/count.js
@@ -4,11 +4,11 @@
 var num1 = 0;
 
 function start(){
-    if(sessionStorage.num){
-        num1 = sessionStorage.num;
-    }
+    //if(sessionStorage.num){
+    //    num1 = sessionStorage.num;
+    //}
     postMessage(num1);
-    sessionStorage.num = num1;
+    //sessionStorage.num = num1;
     num1++;
     setTimeout(start,1000);
 }
